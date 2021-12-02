286b7974c85ae8dcc6c3d0dba17ff5c9180ab57b
cschwa11 <94878998+cschwa11@users.noreply.github.com>
2021-12-01 21:01:28 -0500

    Update main-imagenet.py

--- main-imagenet.py
+++ main-imagenet.py
@@ -232,8 +232,8 @@ transform_train = transforms.Compose([
     transforms.ToPILImage(),
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.ToTensor()
 ])
 
 transform_test = transforms.Compose([
