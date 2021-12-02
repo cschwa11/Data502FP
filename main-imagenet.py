4a72393b7fb34ed9ee40cd07881060e97117876c
cschwa11 <94878998+cschwa11@users.noreply.github.com>
2021-12-01 21:08:17 -0500

    Update main-imagenet.py

--- main-imagenet.py
+++ main-imagenet.py
@@ -229,16 +229,25 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
+    print(type()),
     transforms.ToPILImage(),
+    print(type()),
     transforms.RandomCrop(32, padding=4),
+    print(type()),
     transforms.RandomHorizontalFlip(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    transforms.ToTensor()
+    print(type()),
+    transforms.ToTensor(),
+    print(type()),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    
 ])
 
 transform_test = transforms.Compose([
+    print(type()),
     transforms.ToPILImage(),
+    print(type()),
     transforms.ToTensor(),
+    print(type()),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
