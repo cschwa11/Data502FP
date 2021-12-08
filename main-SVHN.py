4bd5a962affb7a41213bcb5deef788f41ce3ce97
cschwa11 <94878998+cschwa11@users.noreply.github.com>
2021-12-08 17:27:02 -0500

    Update main-SVHN.py

--- main-SVHN.py
+++ main-SVHN.py
@@ -42,12 +42,12 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(
+trainset = torchvision.datasets.SVHN(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(
+testset = torchvision.datasets.SVHN(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
@@ -58,7 +58,7 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 # Model
 print('==> Building model..')
 # net = VGG('VGG19')
-net = ResNet18()
+net = ResNet18_10()
 # net = PreActResNet18()
 # net = GoogLeNet()
 # net = DenseNet121()
