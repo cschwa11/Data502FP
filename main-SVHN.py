5423ca758e9c3ebb0c1f2c1a7fb4758d468aec9f
cschwa11 <94878998+cschwa11@users.noreply.github.com>
2021-12-08 18:07:13 -0500

    Update main-SVHN.py

--- main-SVHN.py
+++ main-SVHN.py
@@ -48,7 +48,7 @@ trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.SVHN(
-    root='./data', split='train', download=True, transform=transform_test)
+    root='./data', split='test', download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
 
