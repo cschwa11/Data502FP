7ba570c43412eb837ee7c47e1011c950db857566
jackiegold <94922982+jackiegold@users.noreply.github.com>
2021-12-10 22:37:35 -0500

    Update main-CIFAR10-edges.py

--- main-CIFAR10-edges.py
+++ main-CIFAR10-edges.py
@@ -1,4 +1,4 @@
-'''Train TinyImageNetDataset with PyTorch.'''
+'''Train CIFAR10 Edges with PyTorch.'''
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -16,7 +16,7 @@ from utils import progress_bar
 
 	
     
-parser = argparse.ArgumentParser(description='PyTorch TinyImageNetDataset Edges Training')
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Edges Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
