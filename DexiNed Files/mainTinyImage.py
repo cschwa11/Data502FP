07a7e4419e77923a2a04d44006aeaf9408a3bbea
cschwa11 <94878998+cschwa11@users.noreply.github.com>
2021-12-15 09:56:22 -0500

    Update mainTinyImage.py

--- DexiNed Files/mainTinyImage.py
+++ DexiNed Files/mainTinyImage.py
@@ -91,7 +91,9 @@ def train_one_epoch(epoch, dataloader, model, criterion, optimizer, device,
     return loss_avg
 
 transform_test = torchvision.transforms.Compose([
-      torchvision.transforms.ToTensor()
+      torchvision.transforms.Resize(32),
+      torchvision.transforms.ToTensor(),
+      torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
 ])
 
 class CustomDataSet(Dataset):
